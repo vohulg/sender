212d950fda5f1cadb2a2b02ec59b4f3b427cffc1
vohulg <vohulg@gmail.com>
2017-11-16 10:50:59 +0500

    Initial Commit

---
+++ sender/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sender
+//
+//  Created by max udin on 11/16/17.
+//  Copyright © 2017 max udin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
